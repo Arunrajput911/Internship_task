d93662c44a7a9f49666ca1ce91c72f18aa9973b4
Arun Rajput <ar1445942@gmail.com>
2021-02-17 18:03:19 +0530

    add

--- index.js
+++
@@ -1,9 +0,0 @@
-const axios =require("axios");
-
-
-axios.post('https://my-notary-app.herokuapp.com/getCustomerDashboard',{ customerId : "6027b462059a1100152ef4db" })
-  .then(function (response) {
-    console.log(response.data);
-  })
-
- 
